fa90387f1a6f909d79b3d36e5bddb5766685e776
mbrishi <bhargavrishi@usf.edu>
2025-07-26 17:37:41 -0400

    Modified main.py to build on render

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,12 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import torch
+import os
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # default fallback for local dev
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 # FastAPI setup
 app = FastAPI()
 
